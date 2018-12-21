7114243cdea5d6b0e4c3eddad8de9b4f476e8cd9
unknown <kiddmit3@gmail.com>
2018-12-21 01:51:22 -0800

    adding name

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,14 @@ import { Link } from 'react-router-dom';
 const Home = props =>
   <div className="home">
     <div>
-      <h1 itemProp="headline">Webrtc Video Room</h1>
+      <h3 itemProp="headline">WebRTC Video Room</h3>
       <p>Please enter a room name.</p>
       <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
       <Link className="primary-button" to={ '/r/' + props.roomId }>Join</Link>
       <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link>
       { props.rooms.length !== 0 && <div>Recently used rooms:</div> }
       { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
+      <h6>by David Lac</h6>
     </div>
   </div>;
 
